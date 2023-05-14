50540a8d9968b104039867302f9b6d4f91a7eaf7
ladhane <40428731+ladhane@users.noreply.github.com>
2023-05-14 15:02:12 +0530

    added cart and search functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,html}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "gradient-x": "gradient-x 1s ease infinite",
+      },
+      keyframes: {
+        "gradient-x": {
+          "0%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "100%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      }
+    },
   },
   plugins: [],
-}
-
+};
