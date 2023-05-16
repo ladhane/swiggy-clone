38a57f19a0f99a65b16e0b246c05f71cf0dc0800
ladhane <40428731+ladhane@users.noreply.github.com>
2023-05-16 10:40:37 +0530

    Add files via upload

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -80,7 +80,7 @@ const RestaurantMenu = () => {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
-          className="w-5 h-5 mr-3"
+          className="w-5 h-5"
         >
           <path
             fillRule="evenodd"
@@ -91,20 +91,6 @@ const RestaurantMenu = () => {
         <h1 className="mr-3 font-extrabold">
           {restaurantInfo.sla.maxDeliveryTime} MINS
         </h1>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-5 h-5 mr-2"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15 8.25H9m6 3H9m3 6l-3-3h1.5a3 3 0 100-6M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
         <h1 className="font-extrabold">{restaurantInfo.costForTwoMessage}</h1>
       </div>
       {restaurantMenu && (
